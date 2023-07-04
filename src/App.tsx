0a68a1f08b413c0fb110232bf7abf0f059d4241e
matcastaneda <matiascmondaca@gmail.com>
2023-07-04 17:55:18 -0400

    feat: remove `ReactQueryDevTools`

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './css/main.css';
 
 import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'sonner';
 import LangProvider from 'providers/localeProvider';
 import ThemeProvider from 'providers/theme/themeProvider';
@@ -24,8 +23,6 @@ const App = () => {
           <Toaster richColors />
         </ThemeProvider>
       </LangProvider>
-
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
