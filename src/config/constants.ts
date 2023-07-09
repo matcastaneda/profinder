97c04a7e1a95fedfc81fa06e114b2ea1cb443b02
matcastaneda <matiascmondaca@gmail.com>
2023-07-09 11:54:16 -0400

    chore: remove github url from .env

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL;
+export const GITHUB_API_URL = 'https://api.github.com';
 export const GITHUB_API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
 
 export const HEADERS = Object.freeze({
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_GITHUB_API_URL: string;
   readonly VITE_GITHUB_API_KEY: string;
 }
 
